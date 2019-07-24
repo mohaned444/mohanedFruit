a17873585b3258052cf357a2e81026c57f997143
mohaned <mohanad.444@hotmail.com>
2019-07-23 20:33:19 -0500

    first commit

---
+++ src/App.kt
@@ -0,0 +1,43 @@
+fun main() {
+//    println("POW")
+//    var x = 3
+//    val name = "Zaid"
+//    x = x * 10
+//    println("x is $")
+//// I'm a comment
+//
+////decrement x by 1
+//    println("x before the while loop is: $x")
+//    while (x > 20) {
+//        x = x - 1
+//
+//        print("$x-")
+//    }
+//    println("x After the FOR loop is: $x")
+//    //condition
+//    if (x == 20) {
+//        println("x must be twenty")
+//    } else {
+//        println("x isn't twenty)
+//    }
+//    var y = 9
+//    when {
+//        x < y -> println("x is small")
+//        x > y 6
+//    }
+
+    val index = arrayOf(1,3,4,2)
+    val fruit = arrayOf("Apple","Banana","cherry","blueberry","pomegranate")
+    var x = 0
+    var y: Int
+    while (x < 4) {
+        y = index [x]
+        x = x + 1
+        println(message = "Fruit = ${fruit[y]}")
+    }
+    }
+
+
+
+
+
